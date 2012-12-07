04edf9245d09374d4949d6030a39cacafa4a97bc
Christian Bodart <chris@bodar.com>
2012-12-07 16:48:27 +0000

    updated tests

--- dev/scripts/tests/dummies/jistkit.extender.js
+++ dev/scripts/tests/dummies/jistkit.extender.js
@@ -3,55 +3,16 @@ JistKit.DummyExtender = function JistKit_DummyExtender(jistkit) {
 }
 JistKit.DummyExtender.prototype = new JistKit();
 JistKit.createOnDemandProperty(JistKit.prototype,"dummy",JistKit.DummyExtender);
-JistKit.extendFromLiteral(JistKit.DummyExtender,{
-		ownerPropertyName: "jistkit"
-	},
-	{
-		element: {
-			get: function () {
-				return this.jistkit.element;
-			}
-		}
-	}
-
-);
-
-
-
+JistKit.extendFromLiteral(JistKit.DummyExtender);
 
 JistKit.DummyExtender2 = function JistKit_DummyExtender2(jistkit) {
 	JistKit.call(this,jistkit);
 }
 JistKit.DummyExtender2.prototype = Object.create(JistKit.prototype);
 JistKit.createOnDemandProperty(JistKit.prototype,"dummy2",JistKit.DummyExtender2);
-JistKit.extendFromLiteral(JistKit.DummyExtender2,{
-		ownerPropertyName: "jistkit"
-	},
-	{
-		element: {
-			get: function () {
-				return this.jistkit.element;
-			}
-		}
-	}
-
-);
+JistKit.extendFromLiteral(JistKit.DummyExtender2);
 
 JistKit.DummyExtender3 = function JistKit_DummyExtender3(jistkit) {
 	JistKit.call(this,jistkit);
 }
-JistKit.createType(
-	JistKit.DummyExtender3,
-	JistKit,
-	"dummy3",
-	{
-		ownerPropertyName: "jistkit"
-	},
-	{
-		element: {
-			get: function () {
-				return this.jistkit.element;
-			}
-		}
-	}
-)
+JistKit.createType(JistKit.DummyExtender3,"dummy3")
