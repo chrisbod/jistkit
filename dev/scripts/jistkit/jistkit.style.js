c642785aa60247b2ed9adc27aa87bb8f436d725f
Christian Bodart <chris@bodar.com>
2012-12-07 16:15:38 +0000

    refactor/tidy

--- dev/scripts/jistkit/jistkit.style.js
+++ dev/scripts/jistkit/jistkit.style.js
@@ -1,66 +1,72 @@
 
 JistKit.globalStyle = {
-    styleSheet: (function jistKit_globalStyle_styleSheet_get() {
+
+    styleSheet: (function JistKit_globalStyle_styleSheet_get() {
         var element = this.document.head.appendChild(this.document.createElement("style"));
         element.id = "jistkitstylesheet"
         return element.sheet
     })()
     ,
-    prefix: (function jistKit_globalStyle_prefix_get() {
+    prefix: (function JistKit_globalStyle_prefix_get() {
         for (var style = getComputedStyle(document.documentElement,null),i=0;i!=style.length;i++) {
             if (style[i].charAt(0)=='-') {
                 return style[i].match(/-[^-]+-/)[0]
             }
         }
     })(),
+
     ruleCache: {},
     propertyCache: {},
     idCount: 0,
-    addRuleDefinition: function jistKit_globalStyle_addRuleDefinition(selector,definitionObject) {
-        if (selector.charAt(0)=="@") {
-            return this.addNestedRuleDefinition(selector,definitionObject);
-        }
-        this.styleSheet.insertRule(selector+"{}",this.styleSheet.cssRules.length);
-        var rule = this.styleSheet.cssRules[this.styleSheet.cssRules.length-1],
-            style = rule.style,
-            propertyName,
-            propertyType;
-        for (propertyName in definitionObject) {
-                this.setStylePropertyValue(style,propertyName,definitionObject[propertyName]);
-        }
-        this.storeRuleInCache(selector,rule);
-        return rule;
+
+    generateId: function JistKit_globalStyle_generateId(prefix) {
+        prefix = prefix || "jistkitElement";
+        return prefix+this.idCount++;
     },
-    addNestedRuleDefinition: function jistKit_globalStyle_addNestedRuleDefinition(selector,definitionObject) {
-        var declarations = selector.trim().split(' ');
+    
+    addRuleDefinition: function JistKit_globalStyle_addRuleDefinition(selector,definitionObject) {
+        if (selector.charAt(0)=="@") {
+            return this.addNestedRuleDefinitions(selector,definitionObject);
+        } else {
+            this.styleSheet.insertRule(selector+"{}",this.styleSheet.cssRules.length);
+            var rule = this.styleSheet.cssRules[this.styleSheet.cssRules.length-1];
+            this.defineStyleDeclaration(rule.style,definitionObject)
+            return rule;
+        }
+    },
+    defineStyleDeclaration: function JistKit_defineRuleStyleDeclaration(styleDeclaration, definitionObject) {
+        for (var propertyName in definitionObject) {
+                this.setStylePropertyValue(styleDeclaration,propertyName,definitionObject[propertyName]);
+        }
+    },
+    addNestedRuleDefinitions: function JistKit_globalStyle_addNestedRuleDefinitions(selector, definitionObject) {
+        var declarations = selector.trim().split(' '),
             ruleDeclaration = declarations[0],
-            name = declarations[1]||'';
+            name = declarations[1]||'',
+            cssRules = this.styleSheet.cssRules;
         if (this.propertyCache[ruleDeclaration]) {
             ruleDeclaration = this.propertyCache[ruleDeclaration];
         }
         try {
-            this.styleSheet.insertRule(ruledeclaration+' '+name
-
-                +"{}",this.styleSheet.cssRules.length);
+            this.styleSheet.insertRule(ruleDeclaration+' '+name+"{}",cssRules.length);
         } catch (e) {
-            var proprietaryPropertyName = selector.replace("@","@"+this.prefix,"")
+            var proprietaryPropertyName = selector.replace("@","@"+this.prefix,"");
             this.styleSheet.insertRule(proprietaryPropertyName+"{}");
             this.propertyCache[selector.split(' ')[0]] = proprietaryPropertyName.split(' ')[0];
         }
-        var rule = this.styleSheet.cssRules[this.styleSheet.cssRules.length-1];
-        for (var i in definitionObject) {
-            rule.insertRule(i+"{}",rule.cssRules.length);
-            var nestedRule = rule.cssRules[rule.cssRules.length-1];
-            for (var j in definitionObject[i]) {
-                this.setStylePropertyValue(nestedRule.style,j,definitionObject[i][j]);
-            }
-        }
-
+        this.addNestedRuleDefinition(cssRules[cssRules.length-1], definitionObject);
     },
-    setRulePropertyValue: function jistKit_globalStyle_setRulePropertyValue(rule,propertyName,value) {
+    addNestedRuleDefinition: function JistKit_globalStyle_addNestedRuleDefinition(parentRule,definitionObject) {
+        var cssRules = parentRule.cssRules;
+        for (var propertyName in definitionObject) {
+            parentRule.insertRule(propertyName+"{}",cssRules.length);
+            this.defineStyleDeclaration(cssRules[cssRules.length-1].style,definitionObject[propertyName]);
+        }
+    },
+    setRulePropertyValue: function JistKit_globalStyle_setRulePropertyValue(rule,propertyName,value) {
         this.setStylePropertyValue(rule.style,propertyName,value)
     },
-    setStylePropertyValue: function jistKit_globalStyle_setStylePropertyValue(style,propertyName,value) {
+    setStylePropertyValue: function JistKit_globalStyle_setStylePropertyValue(style,propertyName,value) {
         if (typeof value != "string") {
             throw new Error("JistKit.Style: style property ["+propertyName+"] must be string")
         }
@@ -74,8 +80,8 @@ JistKit.globalStyle = {
             }
         }
     },
-    storeRuleInCache: function jistKit_globalStyle_storeRuleInCache(selector,rule) {
-        for (var cache = this.ruleCache,matchingRules,selectors = selector.split(/\s*,\s*/g), i=selectors.length-1;i!=-1;i--) {
+    storeRuleInCache: function JistKit_globalStyle_storeRuleInCache(selector,rule) {
+        for (var cache = this.ruleCache, matchingRules, selectors = selector.split(/\s*,\s*/g), i=selectors.length-1;i!=-1;i--) {
             matchingRules = cache[selectors[i]];
             if (!matchingRules) {
                 cache[selectors[i]] = [rule];
@@ -84,7 +90,7 @@ JistKit.globalStyle = {
             }
         }
     },
-    getRulesBySelector: function jistKit_globalStyle_getRulesBySelector(selector) {
+    getRulesBySelector: function JistKit_globalStyle_getRulesBySelector(selector) {
         var rules = [];
         for (var cache = this.ruleCache, selectors = selector.split(/\s*,\s*/g), i = selectors.length-1; i!=-1;i--) {
             if (cache[selectors[i]]) {
@@ -93,7 +99,7 @@ JistKit.globalStyle = {
         }
         return rules;
     },
-    getRulesThatApplyToElement: function jistKit_globalStyle_getRulesThatApplyToElement(element) {//THIS IS REALLY ONLY FOR DEV - it could easily grind a browser to a halt...
+    getRulesThatApplyToElement: function JistKit_globalStyle_getRulesThatApplyToElement(element) {//THIS IS REALLY ONLY FOR DEV - it could easily grind a browser to a halt...
         var methodName = "matchesSelector" in element ? "matchesSelector" : this.prefix.replace(/-/g,'')+"MatchesSelector",
             rules = [];
         if (element[methodName]) {
@@ -108,13 +114,7 @@ JistKit.globalStyle = {
         }
         return rules;
     },
-    generateId: function jistKit_globalStyle_generateId(prefix) {
-        if (!prefix) {
-            prefix = "jistkitElement"
-        }
-        return prefix+this.idCount++;
-    },
-    dispose: function jistKit_globalStyle_dispose() {
+    dispose: function JistKit_globalStyle_dispose() {
         //TODO!!!!
     }
 }
